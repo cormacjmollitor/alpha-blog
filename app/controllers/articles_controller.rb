df9787ac06a0bd915996b9fcc0f496864442fec8
Cormac <cormacjmollitor@gmail.com>
2017-04-27 23:19:36 +0000

    Fixed create, add flash, and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,34 @@
 class ArticlesController < ApplicationController
+  
+  #Needed to make the page run
+  #This creates a new instance of the class Article called article
   def new
     @article = Article.new
   end
   
+  #This actually creates the article with data passed in your form fields
+  #These form fields can be found in the new.html.erb file in the articles folder in views
   def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    #render plain: params[:article].inspect #This line straight up shows the entry you submitted on the browser
+    @article = Article.new(article_params) #This creates a new article with the parameters you passed thru the form
+    #@article.save #This saves the new article to the database table
+    #redirect_to article_path(@article) 
+    
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    #else is taken if the validations for article failed
+    else
+      render 'new'
+    end
+  end
+  
+  def show 
+    @article = Article.find(params[:id])
   end
   
   private
+  #This gets the article parameters from the form
     def article_params
       params.require(:article).permit(:title, :description)  
     end
